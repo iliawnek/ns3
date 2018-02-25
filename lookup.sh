275996fcb7dfdfd72c6ce0e2878ad08579779535
Ken Li <iliawnek@gmail.com>
2018-02-25 18:32:56 +0000

    lookup.sh

---
+++ lookup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# google.com
+# todoist.com
+# amazon.co.uk
+# apple.com
+# 
+# hku.hk
+# unibo.itz
+# unisa.ac.za
+# singaporetech.edu.sg
+# 
+# vk.com
+# qq.com
+# weibo.com
+# twitter.com
+# 
+# smh.com.au
+# estadao.com.br
+# bbc.co.uk
+# thenews.mx
+# 
+# jessicahische.is
+# ken.li
+# kevinclark.ca
+# rafa.design
+
+./dnslookup google.com todoist.com amazon.co.uk apple.com hku.hk unibo.itz unisa.ac.za singaporetech.edu.sg vk.com qq.com weibo.com twitter.com smh.com.au estadao.com.br bbc.co.uk thenews.mx jessicahische.is ken.li kevinclark.ca rafa.design > lookup.txt
